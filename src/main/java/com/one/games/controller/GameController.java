b6d48893662116f3927614a1d6f1d2ec71e457ee
ldd1278073960 <d1278073960>
2019-08-27 09:20:51 +0800

    稍微修改了画廊

--- src/main/java/com/one/games/controller/GameController.java
+++ src/main/java/com/one/games/controller/GameController.java
@@ -11,6 +11,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 /**
+ * 图片画廊
  * @author Administrator
  */
 @RestController
--- src/main/java/com/one/games/domain/entity/GameConf.java
+++ src/main/java/com/one/games/domain/entity/GameConf.java
@@ -1,5 +1,9 @@
 package com.one.games.domain.entity;
 
+
+import lombok.Data;
+
+@Data
 public class GameConf {
     /**
     * 游戏配置ID
@@ -61,99 +65,8 @@ public class GameConf {
     */
     private String gameConfLowInter;
 
-    public Integer getGameConfId() {
-        return gameConfId;
-    }
-
-    public void setGameConfId(Integer gameConfId) {
-        this.gameConfId = gameConfId;
-    }
-
-    public Integer getGameDetailsId() {
-        return gameDetailsId;
-    }
-
-    public void setGameDetailsId(Integer gameDetailsId) {
-        this.gameDetailsId = gameDetailsId;
-    }
-
-    public String getGameConfRecomOs() {
-        return gameConfRecomOs;
-    }
-
-    public void setGameConfRecomOs(String gameConfRecomOs) {
-        this.gameConfRecomOs = gameConfRecomOs;
-    }
-
-    public String getGameConfRecomCpu() {
-        return gameConfRecomCpu;
-    }
-
-    public void setGameConfRecomCpu(String gameConfRecomCpu) {
-        this.gameConfRecomCpu = gameConfRecomCpu;
-    }
-
-    public String getGameConfRecomMem() {
-        return gameConfRecomMem;
-    }
-
-    public void setGameConfRecomMem(String gameConfRecomMem) {
-        this.gameConfRecomMem = gameConfRecomMem;
-    }
-
-    public String getGameConfRecomGra() {
-        return gameConfRecomGra;
-    }
-
-    public void setGameConfRecomGra(String gameConfRecomGra) {
-        this.gameConfRecomGra = gameConfRecomGra;
-    }
-
-    public String getGameConfRecomInter() {
-        return gameConfRecomInter;
-    }
-
-    public void setGameConfRecomInter(String gameConfRecomInter) {
-        this.gameConfRecomInter = gameConfRecomInter;
-    }
-
-    public String getGameConfLowOs() {
-        return gameConfLowOs;
-    }
-
-    public void setGameConfLowOs(String gameConfLowOs) {
-        this.gameConfLowOs = gameConfLowOs;
-    }
-
-    public String getGameConfLowCpu() {
-        return gameConfLowCpu;
-    }
-
-    public void setGameConfLowCpu(String gameConfLowCpu) {
-        this.gameConfLowCpu = gameConfLowCpu;
-    }
-
-    public String getGameConfLowMem() {
-        return gameConfLowMem;
-    }
-
-    public void setGameConfLowMem(String gameConfLowMem) {
-        this.gameConfLowMem = gameConfLowMem;
-    }
-
-    public String getGameConfLowGar() {
-        return gameConfLowGar;
-    }
-
-    public void setGameConfLowGar(String gameConfLowGar) {
-        this.gameConfLowGar = gameConfLowGar;
-    }
-
-    public String getGameConfLowInter() {
-        return gameConfLowInter;
-    }
-
-    public void setGameConfLowInter(String gameConfLowInter) {
-        this.gameConfLowInter = gameConfLowInter;
-    }
+    /**
+     * game外键
+     */
+    private Integer gameId;
 }
